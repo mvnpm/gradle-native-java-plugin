0ccb816fbec54333ce1943f15fd687d0a327e091
Schalk W. Cronjé <ysb33r@gmail.com>
2026-08-28 12:41:24 +0200

    Add support for Settings plugins
    - Refactored some of the logic into static methods on
      NativeJavaPLugin.
    - Added a settings plugin which manipulates the buildscript
      classpath so that plugin resolution can also use native
      variants.

--- src/main/java/io/mvnpm/gradle/plugin/NativeJavaPlugin.java
+++ src/main/java/io/mvnpm/gradle/plugin/NativeJavaPlugin.java
@@ -2,12 +2,16 @@ package io.mvnpm.gradle.plugin;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.attributes.AttributeContainer;
+import org.gradle.api.logging.Logger;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.internal.os.OperatingSystem;
 import org.gradle.nativeplatform.MachineArchitecture;
 import org.gradle.nativeplatform.OperatingSystemFamily;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 /**
  * Automatically sets the following attributes on all Gradle configurations:
@@ -23,45 +27,57 @@ public class NativeJavaPlugin implements Plugin<@NotNull Project> {
     @Override
     public void apply(Project project) {
         ObjectFactory objects = project.getObjects();
-        final OperatingSystemFamily detectedOs;
-        OperatingSystem os = OperatingSystem.current();
-        if (os.isMacOsX()) {
-            detectedOs = objects.named(OperatingSystemFamily.class, OperatingSystemFamily.MACOS);
-        } else if (os.isLinux()) {
-            detectedOs = objects.named(OperatingSystemFamily.class, OperatingSystemFamily.LINUX);
-        } else if (os.isWindows()) {
-            detectedOs = objects.named(OperatingSystemFamily.class, OperatingSystemFamily.WINDOWS);
-        } else {
-            project.getLogger().warn("Native Java plugin - Unsupported operating system: {}", os);
-            detectedOs = null;
-        }
-        String systemArch = System.getProperty("os.arch").toLowerCase();
-        MachineArchitecture detectedArch;
-        if (systemArch.contains("aarch64") || systemArch.contains("arm64")) {
-            detectedArch = objects.named(MachineArchitecture.class, MachineArchitecture.ARM64);
-        } else if (systemArch.contains("64")) {
-            detectedArch = objects.named(MachineArchitecture.class, MachineArchitecture.X86_64);
-        } else if (systemArch.contains("x86")) {
-            detectedArch = objects.named(MachineArchitecture.class, MachineArchitecture.X86);
-        } else {
-            project.getLogger().warn("Native Java plugin - Unsupported architecture: {}", systemArch);
-            detectedArch = null;
-        }
+        Optional<Logger> logger = Optional.of(project.getLogger());
+        final OperatingSystemFamily detectedOs = getOperatingSystemFamily(objects, logger);
+        final MachineArchitecture detectedArch = getMachineArchitecture(objects, logger);
+
         if (detectedOs != null || detectedArch != null) {
             project.getLogger().info("Native Java plugin - applied with: {} and {}", detectedOs, detectedArch);
             project.getConfigurations().configureEach(configuration -> {
 
                 if (configuration.isCanBeResolved()) {
-                    AttributeContainer attributes = configuration.getAttributes();
-
-                    if (detectedOs != null && !attributes.contains(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE)) {
-                        attributes.attribute(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE, detectedOs);
-                    }
-                    if (detectedArch != null && !attributes.contains(MachineArchitecture.ARCHITECTURE_ATTRIBUTE)) {
-                        attributes.attribute(MachineArchitecture.ARCHITECTURE_ATTRIBUTE, detectedArch);
-                    }
+                    addAttributes(configuration, detectedOs, detectedArch);
                 }
             });
+        }
+    }
+
+    static OperatingSystemFamily getOperatingSystemFamily(ObjectFactory objects, Optional<Logger> logger) {
+        final OperatingSystem os = OperatingSystem.current();
+        if (os.isMacOsX()) {
+            return objects.named(OperatingSystemFamily.class, OperatingSystemFamily.MACOS);
+        } else if (os.isLinux()) {
+            return objects.named(OperatingSystemFamily.class, OperatingSystemFamily.LINUX);
+        } else if (os.isWindows()) {
+            return objects.named(OperatingSystemFamily.class, OperatingSystemFamily.WINDOWS);
+        } else {
+            logger.ifPresent(x -> x.warn("Native Java plugin - Unsupported operating system: {}", os));
+            return null;
+        }
+    }
+
+    static MachineArchitecture getMachineArchitecture(ObjectFactory objects, Optional<Logger> logger) {
+        String systemArch = System.getProperty("os.arch").toLowerCase();
+        if (systemArch.contains("aarch64") || systemArch.contains("arm64")) {
+            return objects.named(MachineArchitecture.class, MachineArchitecture.ARM64);
+        } else if (systemArch.contains("64")) {
+            return objects.named(MachineArchitecture.class, MachineArchitecture.X86_64);
+        } else if (systemArch.contains("x86")) {
+            return objects.named(MachineArchitecture.class, MachineArchitecture.X86);
+        } else {
+            logger.ifPresent(x -> x.warn("Native Java plugin - Unsupported architecture: {}", systemArch));
+            return null;
+        }
+    }
+
+    static void addAttributes(Configuration configuration, OperatingSystemFamily detectedOs, MachineArchitecture detectedArch) {
+        AttributeContainer attributes = configuration.getAttributes();
+
+        if (detectedOs != null && !attributes.contains(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE)) {
+            attributes.attribute(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE, detectedOs);
+        }
+        if (detectedArch != null && !attributes.contains(MachineArchitecture.ARCHITECTURE_ATTRIBUTE)) {
+            attributes.attribute(MachineArchitecture.ARCHITECTURE_ATTRIBUTE, detectedArch);
         }
     }
 }
